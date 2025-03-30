abdb2b10cb44db5dcba804c542a1dfc106a4a568
Arthur Falque Pierrotin <arthur.fp.dev@pm.me>
2025-03-31 01:21:18 +0200

    :recycle: Cleaning up the TeamValidationException

--- symfony/src/Exception/TeamValidationException.php
+++ symfony/src/Exception/TeamValidationException.php
@@ -4,16 +4,11 @@ namespace App\Exception;
 
 class TeamValidationException extends \DomainException
 {
-    private array $context;
+    private(set) array $context;
 
-    public function __construct(string $message = "", int $code = 0, \Throwable $previous = null, array $context = [])
+    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null, array $context = [])
     {
         parent::__construct($message, $code, $previous);
         $this->context = $context;
-    }
-
-    public function getContext(): array
-    {
-        return $this->context;
     }
 }
