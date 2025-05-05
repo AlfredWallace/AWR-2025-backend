c8356fd6d5380ee343f94f529f311fba0de84168
Arthur Falque Pierrotin <arthur.fp.dev@pm.me>
2025-05-05 18:22:08 +0200

    :card_file_box: Remove `points_exchanged` column from `rugby_match` via migration

---
+++ symfony/migrations/Version20250505162126.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250505162126 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE rugby_match DROP points_exchanged');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE rugby_match ADD points_exchanged DOUBLE PRECISION NOT NULL');
+    }
+}
