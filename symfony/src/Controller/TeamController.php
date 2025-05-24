9d6a77b409eea0c97867b437754447b3ce39198e
Arthur Falque Pierrotin <arthur.fp.dev@pm.me>
2025-05-25 00:18:45 +0200

    :sparkles: Add reset teams endpoint and update smoke tests

--- symfony/src/Controller/TeamController.php
+++ symfony/src/Controller/TeamController.php
@@ -3,15 +3,22 @@
 namespace App\Controller;
 
 use App\Repository\TeamRepository;
+use App\Service\ResetTeams;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 #[Route('/api', name: 'api_')]
 class TeamController extends AbstractController
 {
     public function __construct(
-        private readonly TeamRepository $teamRepository
+        private readonly TeamRepository $teamRepository,
+        private readonly ResetTeams $resetTeamsService
     ) {
     }
 
@@ -20,5 +27,23 @@ class TeamController extends AbstractController
     {
         $teams = $this->teamRepository->findAll();
         return $this->json(['teams' => $teams]);
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    #[Route('/teams/reset', name: 'reset_teams', methods: ['POST'])]
+    public function resetTeams(): JsonResponse
+    {
+        try {
+            $this->resetTeamsService->resetTeams();
+            return $this->json(['message' => 'Teams have been successfully reset']);
+        } catch (\Exception $e) {
+            return $this->json(['error' => 'An error occurred while resetting teams: ' . $e->getMessage()], 500);
+        }
     }
 }
