69fd8e9a4f815575d7d8bcd29ae7ac03c4cefe56
Arthur Falque Pierrotin <arthur.fp.dev@pm.me>
2025-05-25 17:25:13 +0200

    :ambulance: Add serializer ignore attribute to the User password field

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Attribute as Serializer;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -28,10 +29,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private array $roles = [];
 
-    /**
-     * @var ?string The hashed password
-     */
     #[ORM\Column]
+    #[Serializer\Ignore]
     private ?string $password = null;
 
     #[ORM\OneToMany(targetEntity: Simulation::class, mappedBy: 'user', orphanRemoval: true)]
