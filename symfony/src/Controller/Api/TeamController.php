1d606aa87713820f5594d5dc8164ff2d7055483a
Arthur Falque Pierrotin <arthur.fp.dev@pm.me>
2025-05-31 15:47:28 +0200

    :recycle: Replace `Annotation\Route` with `Attribute\Route` across controllers

--- symfony/src/Controller/Api/TeamController.php
+++ symfony/src/Controller/Api/TeamController.php
@@ -7,7 +7,7 @@ use App\Service\ResetTeams;
 use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
--- symfony/src/Controller/Api/UserController.php
+++ symfony/src/Controller/Api/UserController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api', name: 'api_')]
 #[OA\Tag(name: 'Users', description: 'Operations related to users')]
